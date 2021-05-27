23b9c5c23cbe523ffebd02ad39a7e3be5edba753
ASagaida1 <sagaidaalexandr@gmail.com>
2021-05-27 22:18:44 +0300

    init

---
+++ src/main/java/org/asa/spring/spring_introduction/Dog.java
@@ -0,0 +1,7 @@
+package org.asa.spring.spring_introduction;
+
+public class Dog {
+    public void say(){
+        System.out.println("Bow-Wow");
+    }
+}
---
+++ src/main/java/org/asa/spring/spring_introduction/Test.java
@@ -0,0 +1,8 @@
+package org.asa.spring.spring_introduction;
+
+public class Test {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.say();
+    }
+}
